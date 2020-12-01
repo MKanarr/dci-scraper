15e96951eefcf02d22a485ffaf22c48c9df7a77b
mkanarr <matthewkanarr@gmail.com>
2020-11-30 18:10:08 -0600

    refactored code

--- scrape.py
+++ scrape.py
@@ -1,7 +1,10 @@
 import requests
 import csv
+import time
+import threading
 from bs4 import BeautifulSoup
-from threading import Thread
+
+start = time.perf_counter()
 
 def Corps(corpsList, soup):
     for corps in soup.find_all('div', class_='sticky-corps'):
@@ -32,8 +35,8 @@ def CreateCSV(filename, corpsList, generalEffect, visual, music, finalTotal):
         csvwriter.writerow(fields)
         csvwriter.writerows(rows)
 
-def Finals2019():
-    page = requests.get('https://www.dci.org/scores/recap/2019-dci-world-championship-finals')
+def scrape(url):
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     corpsList = []
@@ -58,180 +61,29 @@ def Finals2019():
     visual.reverse()
     music.reverse()
 
-    CreateCSV('2019finals.csv', corpsList, generalEffect, visual, music, finalTotal)
+    filename = url.split('/')[-1] + '.csv'
 
-def Finals2018():
-    page = requests.get('https://www.dci.org/scores/recap/2018-dci-world-championship-finals')
-    soup = BeautifulSoup(page.content, 'html.parser')
+    CreateCSV(filename, corpsList, generalEffect, visual, music, finalTotal)
 
-    corpsList = []
-    rawScores = []
+urls = ['https://www.dci.org/scores/recap/2019-dci-world-championship-finals', 'https://www.dci.org/scores/recap/2018-dci-world-championship-finals',
+        'https://www.dci.org/scores/recap/2017-dci-world-championship-finals', 'https://www.dci.org/scores/recap/2016-dci-world-championships-finals',
+        'https://www.dci.org/scores/recap/2015-dci-world-championship-world-class-finals', 'https://www.dci.org/scores/recap/2014-dci-world-championships-world-class-finals',
+        'https://www.dci.org/scores/recap/2013-dci-world-championship-finals'
+        ]
 
-    # corps names
-    corpsList = Corps(corpsList, soup)
+threads = []
 
-    # subcaption scores
-    rawScores = SubCaptions(rawScores, soup)
+# for url in urls:
+#     scrape(url)
 
-    subTotal = rawScores[3::5]
-    finalTotal = rawScores[4::5]
-    generalEffect = rawScores[0::5]
-    visual = rawScores[1::5]
-    music = rawScores[2::5]
+for url in urls:
+    t = threading.Thread(target=scrape, args=[url])
+    t.start()
+    threads.append(t)
 
-    corpsList.reverse()
-    subTotal.reverse()
-    finalTotal.reverse()
-    generalEffect.reverse()
-    visual.reverse()
-    music.reverse()
+for thread in threads:
+    thread.join()
 
-    CreateCSV('2018finals.csv', corpsList, generalEffect, visual, music, finalTotal)
+finish = time.perf_counter()
 
-def Finals2017():
-    page = requests.get('https://www.dci.org/scores/recap/2017-dci-world-championship-finals')
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    corpsList = []
-    rawScores = []
-
-    # corps names
-    corpsList = Corps(corpsList, soup)
-
-    # subcaption scores
-    rawScores = SubCaptions(rawScores, soup)
-
-    subTotal = rawScores[3::5]
-    finalTotal = rawScores[4::5]
-    generalEffect = rawScores[0::5]
-    visual = rawScores[1::5]
-    music = rawScores[2::5]
-
-    corpsList.reverse()
-    subTotal.reverse()
-    finalTotal.reverse()
-    generalEffect.reverse()
-    visual.reverse()
-    music.reverse()
-
-    CreateCSV('2017finals.csv', corpsList, generalEffect, visual, music, finalTotal)
-
-def Finals2016():
-    page = requests.get('https://www.dci.org/scores/recap/2016-dci-world-championships-finals')
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    corpsList = []
-    rawScores = []
-
-    # corps names
-    corpsList = Corps(corpsList, soup)
-
-    # subcaption scores
-    rawScores = SubCaptions(rawScores, soup)
-
-    subTotal = rawScores[3::5]
-    finalTotal = rawScores[4::5]
-    generalEffect = rawScores[0::5]
-    visual = rawScores[1::5]
-    music = rawScores[2::5]
-
-    corpsList.reverse()
-    subTotal.reverse()
-    finalTotal.reverse()
-    generalEffect.reverse()
-    visual.reverse()
-    music.reverse()
-
-    CreateCSV('2016finals.csv', corpsList, generalEffect, visual, music, finalTotal)
-
-def Finals2015():
-    page = requests.get('https://www.dci.org/scores/recap/2015-dci-world-championship-world-class-finals')
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    corpsList = []
-    rawScores = []
-
-    # corps names
-    corpsList = Corps(corpsList, soup)
-
-    # subcaption scores
-    rawScores = SubCaptions(rawScores, soup)
-
-    subTotal = rawScores[3::5]
-    finalTotal = rawScores[4::5]
-    generalEffect = rawScores[0::5]
-    visual = rawScores[1::5]
-    music = rawScores[2::5]
-
-    corpsList.reverse()
-    subTotal.reverse()
-    finalTotal.reverse()
-    generalEffect.reverse()
-    visual.reverse()
-    music.reverse()
-
-    CreateCSV('2015finals.csv', corpsList, generalEffect, visual, music, finalTotal)
-
-def Finals2014():
-    page = requests.get('https://www.dci.org/scores/recap/2014-dci-world-championships-world-class-finals')
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    corpsList = []
-    rawScores = []
-
-    # corps names
-    corpsList = Corps(corpsList, soup)
-
-    # subcaption scores
-    rawScores = SubCaptions(rawScores, soup)
-
-    subTotal = rawScores[3::5]
-    finalTotal = rawScores[4::5]
-    generalEffect = rawScores[0::5]
-    visual = rawScores[1::5]
-    music = rawScores[2::5]
-
-    corpsList.reverse()
-    subTotal.reverse()
-    finalTotal.reverse()
-    generalEffect.reverse()
-    visual.reverse()
-    music.reverse()
-
-    CreateCSV('2014finals.csv', corpsList, generalEffect, visual, music, finalTotal)
-
-def Finals2013():
-    page = requests.get('https://www.dci.org/scores/recap/2013-dci-world-championship-finals')
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    corpsList = []
-    rawScores = []
-
-    # corps names
-    corpsList = Corps(corpsList, soup)
-
-    # subcaption scores
-    rawScores = SubCaptions(rawScores, soup)
-
-    subTotal = rawScores[3::5]
-    finalTotal = rawScores[4::5]
-    generalEffect = rawScores[0::5]
-    visual = rawScores[1::5]
-    music = rawScores[2::5]
-
-    corpsList.reverse()
-    subTotal.reverse()
-    finalTotal.reverse()
-    generalEffect.reverse()
-    visual.reverse()
-    music.reverse()
-
-    CreateCSV('2013finals.csv', corpsList, generalEffect, visual, music, finalTotal)
-
-Thread(target=Finals2013).start()
-Thread(target=Finals2014).start()
-Thread(target=Finals2015).start()
-Thread(target=Finals2016).start()
-Thread(target=Finals2017).start()
-Thread(target=Finals2018).start()
-Thread(target=Finals2019).start()
+print(f'Finished in {round(finish - start, 2)} second(s)')
